ed85e05c340f8d9e8ca9e3d8eb8d0c13e90b3737
Sultan8707 <240254@astanait.edu.kz>
2025-02-11 00:44:09 +0500

    Load Piece Images and Initial Setup

---
+++ src/main/java/chess/chess/ChessPiece.java
@@ -0,0 +1,15 @@
+package chess.chess;
+
+public class ChessPiece {
+    private final String type;
+    private final boolean isWhite;
+
+    public ChessPiece(String type, boolean isWhite) {
+        this.type = type;
+        this.isWhite = isWhite;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
